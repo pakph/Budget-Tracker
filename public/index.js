4ed37263f8424f9e38b4292add040ffddeaa1e30
unknown <pakphil7@gmail.com>
2020-06-22 00:52:37 -0700

    added a block of code to allow the app to work in offline mode

--- public/index.js
+++ public/index.js
@@ -1,3 +1,9 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/service-worker.js');
+  });
+}
+
 let transactions = [];
 let myChart;
 
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+    entry: "./public/index.js",
+    output: {
+        path: path.resolve(__dirname + "/public/dist"),
+        filename: "bundle.js"
+    },
+    mode: "production",
+    plugins: [
+        new WebpackPwaManifest({
+            filename: "manifest.json",
+            inject: false,
+            fingerprints: false,
+            name: "Budget Tracker App",
+            short_name: "Budget App",
+            theme_color: "#ffffff",
+            background_color: "#ffffff",
+            start_url: "/",
+            display: "standalone",
+
+            icons: [
+                {
+                    src: path.resolve(
+                        __dirname,
+                        "public/icons/icon-512x512.png"
+                        ),
+
+                    size: [192, 512]
+                }
+            ]
+        })
+    ]
+};
+
+module.exports = config;
