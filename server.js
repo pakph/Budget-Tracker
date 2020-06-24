6ead4aa3dedf5c5846bdcbb70162070acf5703c7
unknown <pakphil7@gmail.com>
2020-06-23 20:04:29 -0700

    edited the mongoose.connect line to connect to database

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://test-user-01:mymongodbpassword@ds127362.mlab.com:27362/heroku_dt80wf6l", {
+mongoose.connect("mongodb://test-user-02:mymongodbpassword1@ds127362.mlab.com:27362/heroku_dt80wf6l", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
